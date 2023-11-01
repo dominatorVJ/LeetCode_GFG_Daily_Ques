c9f7af2df14a2e5b8995070b5ccf8a6ad9c642c1
Vishal Jaiswal <88589080+dominatorVJ@users.noreply.github.com>
2023-11-01 08:42:43 +0530

    Time: 21 ms (28.08%), Space: 25.8 MB (27.04%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        unordered_map<int, int> counter;
+        queue<TreeNode*> queue;
+        queue.push(root);
+        
+        while (!queue.empty()) {
+            TreeNode* node = queue.front();
+            queue.pop();
+
+            counter[node->val]++;
+            if (node->left != nullptr) {
+                queue.push(node->left);
+            }
+            if (node->right != nullptr) {
+                queue.push(node->right);
+            }
+        }
+        
+        int maxFreq = 0;
+        for (auto& [key, val] : counter) {
+            maxFreq = max(maxFreq, val);
+        }
+        
+        vector<int> ans;
+        for (auto& [key, val] : counter) {
+            if (val == maxFreq) {
+                ans.push_back(key);
+            } 
+        }
+        
+        return ans;
+    }
+};
