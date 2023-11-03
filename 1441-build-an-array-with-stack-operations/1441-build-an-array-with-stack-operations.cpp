0d2eaa9c73ff5980b2078077a310cd812c17805a
Vishal Jaiswal <88589080+dominatorVJ@users.noreply.github.com>
2023-11-03 07:59:18 +0530

    Time: 2 ms (47.56%), Space: 8.2 MB (22.92%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,38 +1,47 @@
-class Solution {
-public:
-    vector<string> buildArray(vector<int>& target, int n) {
-        vector<string> ans;
-        vector<int> stream(n+1);
-        for(int i=1;i<=n;i++){
-            stream[i] = i;
-        }
-        stack<int> st;
-        int j = 0;
-        int i = 1;
-        while(j<target.size()){
-            if(target[j] == stream[i]){
-                while(j>0 && target[j-1] != st.top()){
-                    ans.push_back("Pop");
-                    st.pop();
-                }
-                if(j-1<0){
-                    while(!st.empty()){
-                        ans.push_back("Pop");
-                    st.pop();
-                    }
-                }
-                st.push(stream[i]);
-                ans.push_back("Push");
-                i++;
-                j++;
-            }else{
-                st.push(stream[i]);
-                ans.push_back("Push");
-                i++;
+class Solution
+{
+    public:
+        vector<string> buildArray(vector<int> &target, int n)
+        {
+            vector<string> ans;
+            vector<int> stream(n + 1);
+            for (int i = 1; i <= n; i++)
+            {
+                stream[i] = i;
             }
+            stack<int> st;
+            int j = 0;
+            int i = 1;
+            while (j < target.size())
+            {
+                if (target[j] == stream[i])
+                {
+                    while (j > 0 && target[j - 1] != st.top())
+                    {
+                        ans.push_back("Pop");
+                        st.pop();
+                    }
+                    if (j - 1 < 0)
+                    {
+                        while (!st.empty())
+                        {
+                            ans.push_back("Pop");
+                            st.pop();
+                        }
+                    }
+                    st.push(stream[i]);
+                    ans.push_back("Push");
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    st.push(stream[i]);
+                    ans.push_back("Push");
+                    i++;
+                }
+            }
+
+            return ans;
         }
-        
-        
-        return ans;
-    }
 };
