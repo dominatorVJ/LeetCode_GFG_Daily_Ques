4b5d1d5365359b075ed658125eb255acda1e8e77
Vishal Jaiswal <88589080+dominatorVJ@users.noreply.github.com>
2023-12-05 20:14:28 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (98.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,28 +1,36 @@
-class Solution
-{
-    public:
-        void sortColors(vector<int> &nums)
-        {
-            int n = nums.size();
-            int i = 0, j = 0, k = n - 1;
-
-            while (i <= k)
-            {
-                if (nums[i] == 0)
-                {
-                    swap(nums[i], nums[j]);
-                    i++;
-                    j++;
-                }
-                else if (nums[i] == 1)
-                {
-                    i++;
-                }
-                else
-                {
-                    swap(nums[i], nums[k]);
-                    k--;
-                }
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //Approach 1 Normal Sort T.C. - O(nlogn)
+        // sort(nums.begin(),nums.end());
+        //Approach 2 Count Sort T.C. - O(n)
+        // int cnt0 = 0,cnt1 = 0, cnt2=0;
+        // for(int i:nums){
+        //     if(i==0) cnt0++;
+        //     else if(i==1)cnt1++;
+        //     else cnt2++;
+        // }
+        // int i = 0;
+        // while(cnt0--){
+        //     nums[i++]=0;
+        // }
+        // while(cnt1--){
+        //     nums[i++]=1;
+        // }
+        // while(cnt2--){
+        //     nums[i++]=2;
+        // }
+        // Approach 3 DNF Algo
+        int low = 0, mid = 0, high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[mid++], nums[low++] );
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[high--]);
             }
         }
+        
+    }
 };
