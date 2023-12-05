c56694d8819a1d4641b1a9d2a6a958b209d0a851
Vishal Jaiswal <88589080+dominatorVJ@users.noreply.github.com>
2023-12-05 23:50:05 +0530

    Time: 80 ms (73.17%), Space: 61.5 MB (89.25%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,20 +1,28 @@
-class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
-        int i=0,n=nums.size();
-        while(i<n){
-            int cidx = nums[i]-1;
-            if(nums[i] != nums[cidx]){
-                swap(nums[i],nums[cidx]);
-            }else{
-                i++;
+class Solution
+{
+    public:
+        int findDuplicate(vector<int> &nums)
+        {
+           int i = 0, n = nums.size();
+            while (i < n)
+            {
+                int cidx = nums[i] - 1;
+                if (nums[i] != nums[cidx])
+                {
+                    swap(nums[i], nums[cidx]);
+                }
+                else
+                {
+                    i++;
+                }
             }
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i] != i+1){
-                return nums[i];
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] != i + 1)
+                {
+                    return nums[i];
+                }
             }
+            return -1;
         }
-        return -1;
-    }
 };
