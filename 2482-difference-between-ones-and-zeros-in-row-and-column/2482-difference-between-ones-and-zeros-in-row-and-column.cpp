cc1a87ec87d2bd728800d3b2094832a22034522c
Vishal Jaiswal <88589080+dominatorVJ@users.noreply.github.com>
2023-12-14 12:01:51 +0530

    Time: 188 ms (76.75%), Space: 117.6 MB (65.73%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,27 +1,36 @@
-class Solution {
-public:
-    vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> ans(m,vector<int>(n,0));
-        vector<int> rows1(m),rows0(m);
-        vector<int> cols1(n),cols0(n);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++ ){
-                if(grid[i][j] == 0){
-                    rows0[i]++;
-                    cols0[j]++;
-                }else{
-                     rows1[i]++;
-                     cols1[j]++;
+/*
+Simply evaluate no of 1's in row and col by taking the sum of the row and col.
+Then evaluate the diff matrix.
+To evaluate 0's count, subtract sum from the size of the row or col
+so total value of diff[i][j] = row[i] + col[j] - (grid.size()- row[i]) - (grid[0].size() - col[j]) 
+above equation can be simplified as : diff[i][j] = 2*row[i] + 2*col[j] - grid.size() - grid[0].size();
+*/
+
+class Solution
+{
+    public:
+        vector<vector < int>> onesMinusZeros(vector<vector < int>> &grid)
+        {
+            int m = grid.size();
+            int n = grid[0].size();
+            vector<int> row(m);
+            vector<int> col(n);
+            for (int i = 0; i < m; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    row[i] += grid[i][j];
+                    col[j] += grid[i][j];
                 }
             }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans[i][j] = rows1[i]+cols1[j]-rows0[i]-cols0[j];
+            vector<vector < int>> diff(m, vector<int> (n, 0));
+            for (int i = 0; i < m; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    diff[i][j] = 2 *row[i] + 2 *col[j] - m - n;
+                }
             }
+            return diff;
         }
-        return ans;
-    }
 };
