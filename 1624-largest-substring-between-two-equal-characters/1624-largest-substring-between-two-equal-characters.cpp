5f053214fab0016f19bf314af5acbfeb0936990f
Vishal Jaiswal <88589080+dominatorVJ@users.noreply.github.com>
2023-12-31 21:09:04 +0530

    Time: 4 ms (16.71%), Space: 6.8 MB (41.15%) - LeetHub

---
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+        int maxLengthBetweenEqualCharacters(string s)
+        {
+            int n = s.size();
+            int maxi = -1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        maxi = max(maxi, j - i-1);
+                    }
+                }
+            }
+            return maxi;
+        }
+};
